70858fa7821fd6c622faf74e7247d69acad6e5e7
WangZhuo2000 <zhuowangy2k@outlook.com>
2019-12-03 21:58:13 +0800

    change setup information

--- setup.py
+++ setup.py
@@ -7,11 +7,16 @@ from setuptools import setup
 
 setup(
     name='ioctf',
-    version='0.1.0',
+    version='0.1.1',
     author='Zhuo Wang',
     author_email='zhuowangy2k@outlook.com',
     url="https://github.com/WangZhuo2000/ioctf",
     description= 'a simple io package for ctf use python3',
+    long_description='The website is [here](https://github.com/WangZhuo2000/ioctf)',
+    long_description_content_type='text/markdown',
     packages=['ioctf'],
     install_requires=[]
 )
+
+# twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+# python3 setup.py sdist bdist_wheel
